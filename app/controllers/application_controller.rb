1bccde1abc566425d1c5469f98480e8f4f6bd1a3
Grzegorz Miszewski <miszmaniac@gmail.com>
2012-06-07 08:12:31 +0200

    Utworzenie uzytkownikow i wgranie bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
+# -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+   before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || :pl
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606062113) do
+ActiveRecord::Schema.define(:version => 20120607053446) do
 
   create_table "bets", :force => true do |t|
     t.string   "user"
@@ -32,5 +32,18 @@ ActiveRecord::Schema.define(:version => 20120606062113) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin",           :default => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
